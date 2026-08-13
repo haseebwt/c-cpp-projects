df55a64eeff2c05b1c1e96acb2d4f4675a4e1486
beeb <haseeb08.ah@gmail.com>
2026-08-14 02:09:54 +0500

    Game of Life

--- game_of_life/gol.cpp
+++ game_of_life/gol.cpp
@@ -1,171 +1,113 @@
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_pixels.h>
-#include <SDL3/SDL_stdinc.h>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <raylib.h>
 #include <vector>
 
-#define WIDTH 620
-#define HEIGHT 620
-#define RES 15
+#define WIDTH 1280
+#define HEIGHT 720
+#define RES 20
 #define COLS WIDTH / RES
-#define ROWS HEIGHT / RES
+#define ROWS WIDTH / RES
 
-void initCells ();
-
+std::vector<std::vector<int>> make2DArray ();
 bool withinBounds (int val, int counter);
-
-struct Cell
-{
-  int gX = 0, gY = 0;
-  bool alive = false;
-  SDL_Rect pos;
-
-  Cell (int x, int y, bool isAlive)
-  {
-    gX = x;
-    gY = y;
-    alive = isAlive;
-    pos = SDL_Rect{ 0, 0, RES, RES };
-  }
-
-  void
-  show (SDL_Surface *mSurface)
-  {
-    // Uint32 teal = SDL_MapRGB (SDL_GetPixelFormatDetails (mSurface->format),
-    // nullptr, 109, 249, 156);
-    Uint32 white = SDL_MapRGB (SDL_GetPixelFormatDetails (mSurface->format),
-                               nullptr, 255, 255, 255);
-    // pos = SDL_Rect{ gX * RES, gY * RES, RES, RES
-    // };
-    pos.x = gX * RES;
-    pos.y = gY * RES;
-    SDL_FillSurfaceRect (mSurface, &pos, white);
-  }
-};
-
-int checkAlive (Cell cell, std::vector<std::vector<Cell>> grid, int mX,
-                int mY);
-
-std::vector<std::vector<Cell>>
-make2DArray ()
-{
-  std::vector<std::vector<Cell>> vec (
-      COLS, std::vector<Cell> (ROWS, Cell (-1, -1, false)));
-  return vec;
-}
+std::vector<std::vector<int>> initCells (std::vector<std::vector<int>> grid,
+                                         int amount);
+int countNeighbors (std::vector<std::vector<int>> *grid, int mX,
+                    int mY); // m = local idk
 
 int
 main ()
 {
   srand (time (nullptr));
-  if (!SDL_Init (SDL_INIT_VIDEO))
-    {
-      SDL_Log ("Init error: %s", SDL_GetError ());
-    }
 
-  SDL_Window *gWindow;
-  SDL_Renderer *gRenderer;
-  SDL_CreateWindowAndRenderer ("Life of Game", WIDTH, HEIGHT, 0, &gWindow,
-                               &gRenderer);
-  SDL_Surface *gSurface = SDL_GetWindowSurface (gWindow);
+  std::vector<std::vector<int>> grid = make2DArray ();
+  std::vector<std::vector<int>> newGrid;
 
-  std::vector<std::vector<Cell>> grid = make2DArray ();
+  InitWindow (WIDTH, HEIGHT, "Game of Life");
+  SetTargetFPS (30);
 
-  if (!gWindow || !gRenderer)
-    {
-      SDL_Log ("Creation error: %s", SDL_GetError ());
-    }
+  grid = initCells (grid, 300);
 
-  bool isRunning{ true };
-  SDL_Event event;
-
-  // for (int i = 0; i < 10; i++)
-  //   {
-  //     int mX = rand () % COLS - 1;
-  //     int mY = rand () % ROWS - 1;
-  //     if (withinBounds (mX, WIDTH) && withinBounds (mY, HEIGHT)
-  //         && withinBounds (mX / RES, COLS) && withinBounds (mY / ROWS,
-  //         ROWS))
-  //       {
-  //         Cell cell = Cell (mX, mY, true);
-  //         grid[mX][mY] = cell;
-  //       }
-  //   }
-
+  bool isRunning = true;
   while (isRunning)
     {
-      if (SDL_PollEvent (&event))
+      if (IsKeyPressed (KEY_X))
         {
-          if (event.type == SDL_EVENT_KEY_DOWN && event.key.key == SDLK_X)
-            {
-              isRunning = false;
-            }
+          isRunning = false;
+        }
+      if (IsKeyPressed (KEY_SPACE))
+        {
+          grid = initCells (grid, 300);
+        }
 
-          if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN
-              && event.button.button == 1)
-            {
-              float mX = 0, mY = 0;
-              SDL_GetMouseState (&mX, &mY);
-              if (withinBounds (mX, WIDTH) && withinBounds (mY, HEIGHT)
-                  && withinBounds (mX / RES, COLS)
-                  && withinBounds (mY / ROWS, ROWS))
-                {
-                  mX = mX / RES;
-                  mY = mY / RES;
+      if (IsMouseButtonDown (MOUSE_LEFT_BUTTON))
+        {
+          Vector2 mouseState = GetMousePosition ();
 
-                  Cell cell = Cell (mX, mY, true);
-                  grid[mX][mY] = cell;
-                }
+          int mX, mY;
+          mX = mouseState.x / RES;
+          mY = mouseState.y / RES;
+
+          if (withinBounds (mouseState.x, WIDTH)
+              && withinBounds (mouseState.y, HEIGHT) && withinBounds (mX, COLS)
+              && withinBounds (mY, ROWS))
+            {
+              grid[mX][mY] = 1;
             }
         }
 
+      BeginDrawing ();
+      ClearBackground (DARKBLUE);
       for (int x = 0; x < COLS; x++)
         {
           for (int y = 0; y < ROWS; y++)
             {
-              Cell cell = grid[x][y];
-              if (cell.alive)
+              if (grid[x][y] > 0)
                 {
-                  cell.show (gSurface);
+                  DrawRectangle (x * RES, y * RES, RES, RES, PINK);
                 }
+              DrawRectangleLines (x * RES, y * RES, RES, RES,
+                                  DARKGREEN); // stroke
             }
         }
+      EndDrawing ();
 
+      // run logic
+      newGrid = make2DArray ();
       for (int x = 0; x < COLS; x++)
         {
           for (int y = 0; y < ROWS; y++)
             {
-              if (grid[x][y].alive)
+              int cell = grid[x][y];
+              int neighbors = countNeighbors (&grid, x, y);
+              if (neighbors < 2 || neighbors > 3)
                 {
-                  int aliveCount = checkAlive (grid[x][y], grid, x, y);
-                  if (aliveCount == 3)
-                    {
-                      std::cout << "check\n";
-                    }
-                  if (aliveCount < 2)
-                    {
-                      grid[x][y].alive = false;
-                      ;
-                    }
-                  if (aliveCount > 3)
-                    {
-                      grid[x][y].alive = false;
-                    }
-                  if (aliveCount == 3)
-                    {
-                      grid[x][y].alive = true;
-                      std::cout << grid[x][y].alive << "\n";
-                    }
+                  newGrid[x][y] = 0;
+                }
+              else if (cell == 0 && neighbors == 3)
+                {
+                  newGrid[x][y] = 1;
+                }
+              else
+                {
+                  newGrid[x][y] = cell;
                 }
             }
         }
-
-      SDL_UpdateWindowSurface (gWindow);
+      grid = newGrid;
     }
-  std::cout << "Hello World!\n";
+
+  std::cout << "Game of Life\n";
   return 0;
+}
+
+std::vector<std::vector<int>>
+make2DArray ()
+{
+  std::vector<std::vector<int>> vec (COLS, std::vector<int> (ROWS, 0));
+  return vec;
 }
 
 bool
@@ -174,31 +116,46 @@ withinBounds (int val, int counter)
   return val >= 0 && val <= counter - 1;
 }
 
+std::vector<std::vector<int>>
+initCells (std::vector<std::vector<int>> grid, int amount)
+{
+
+  // spawining some cells
+  for (int i = 0; i < amount; i++)
+    {
+      int x, y;
+      x = rand () % COLS - 1;
+      y = rand () % ROWS - 1;
+
+      if (withinBounds (x, WIDTH) && withinBounds (y, HEIGHT))
+        {
+          grid[x][y] = 1;
+        }
+    }
+  return grid;
+}
+
 int
-checkAlive (Cell cell, std::vector<std::vector<Cell>> grid, int mX, int mY)
+countNeighbors (std::vector<std::vector<int>> *grid, int mX, int mY)
 {
   int aliveCount = 0;
-  int matrix{ 1 }, extent{ 2 };
-  std::cout << mX << " " << mY << "\n";
-  for (int i = -extent; i < matrix; i++)
+  for (int i = -1; i < 2; i++)
     {
-      for (int j = -extent; j < matrix; j++)
+      for (int j = -1; j < 2; j++)
         {
           if (withinBounds (mX + i, COLS) && withinBounds (mY + j, ROWS))
-
             {
-              Cell c = grid[mX + i][mY + j];
-              std::cout << mX + i << " " << mY + j << "\n";
-              if (c.gX == mX && c.gY == mY)
+              int cell = grid->at (mX + i).at (mY + j);
+              if (mX == i && mY == j)
                 {
                   continue;
                 }
-              else if (c.alive)
+              else if (cell > 0)
                 {
                   aliveCount++;
                 }
             }
         }
     }
-  return aliveCount - 1;
+  return aliveCount;
 }
